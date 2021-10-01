a36eef19f0d8d81ee223f5b087965635995b9823
Kiren <kirenp.2004@gmail.com>
2021-10-01 14:13:35 -0300

    Started class based windows

--- automation.py
+++ automation.py
@@ -1,8 +1,24 @@
 import tkinter as tk
 
+class Menu:
+    def __init__(self, master, title, geometry):
+        self.master = master
+        self.master.title(title)
+        self.master.geometry(geometry)
 
-window = tk.Tk()
-window.title("Workflow Automation")
-window.geometry("800x600")
+        #define methods for widgets 
 
-window.mainloop()
+        #Add widgets 
+        Label = tk.Label(self.master, text="Hello World")
+        Label.pack()
+
+
+
+        self.master.mainloop()
+
+def run():
+    root = tk.Tk()
+    app = Menu(root, "Workflow Automation", "800x600")
+
+if __name__=="__main__":
+    run()
