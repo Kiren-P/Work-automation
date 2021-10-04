997d75bb0a0958e8d5c6c4970cd6d5fd560860c5
Kiren <kirenp.2004@gmail.com>
2021-10-04 13:45:03 -0300

    Added some widgets and learned about pack and grid methods

--- automation.py
+++ automation.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.constants import LEFT
 
 class Menu:
     def __init__(self, master, title, geometry):
@@ -7,18 +8,30 @@ class Menu:
         self.master.geometry(geometry)
 
         #define methods for widgets 
+        def browseButton():
+            pass
 
         #Add widgets 
-        Label = tk.Label(self.master, text="Hello World")
-        Label.pack()
+
+        #specify folder to open
+        tk.Label(self.master, text="Specify the folder path:").grid(column=0, row=0, padx=2, sticky="NW")
+        folder_path = tk.Entry(self.master, width=46).grid(column=0, row=1, padx=2)
+        browse_button = tk.Button(self.master, widt=6, text="Browse", command=browseButton).grid(column=1, row=1, padx=2)
+
+
+        #specify enviroment to activate 
+        #specify commands to run
+        #specify additional misc commands (usually project specific)
 
 
 
         self.master.mainloop()
 
+
 def run():
     root = tk.Tk()
-    app = Menu(root, "Workflow Automation", "800x600")
+    window_size = "800x600"
+    app = Menu(root, "Workflow Automation", window_size)
 
 if __name__=="__main__":
     run()
