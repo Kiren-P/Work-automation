f00d7803ac0157135ed2e08188501752c0c8e414
Kiren <kirenp.2004@gmail.com>
2021-10-11 14:08:57 -0300

    set up database file

--- data.py
+++ data.py
@@ -1,16 +1,36 @@
 import sqlite3
 
-connection = sqlite3.connect("paths.db")
+#when this file runs the db will get created 
 
+connection = sqlite3.connect("info.db")
 c = connection.cursor()
 
-c.execute("""CREATE TABLE paths (
-        folder_path text,
-        cd_path text,
-        email text 
-        )""")
+def create_table():
+	"""Creates a table with the columns"""
+
+	c.execute("""CREATE TABLE paths (
+			folder_path TEXT,
+			cd_path TEXT,
+			cmdcommands TEXT,
+			programs TEXT 
+			)""")
+
+def insert_paths():
+	"""Inserts paths into the paths table"""
+
+	# c.execute("""
+	# INSERT INTO paths 
+	# VALUES (?, ?, ?, ?)""" ,(var1, var2, var3, var4)) these are the variables to be imported and inserted
+
+def get_paths():
+	"""Queries the all the paths from paths table"""
+
+	c.execute("""
+	SELECT *
+	FROM paths
+	""")
+	paths = c.fetchall()
+
 
 connection.commit()
 connection.close()
-
-#COLUMNS: FOLDER PATH, PATH TO CD IN, COMMANDS TO RUN. PATH TO EXTRA PROGRAM
